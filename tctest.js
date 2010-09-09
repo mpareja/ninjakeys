a3a57ff7c8649f5f77463d89f2cd160419cfcfac
Mario Pareja <mpareja@lightning.(none)>
2010-09-09 08:50:27 -0400

    Added a bunch of tests.

--- tctest.js
+++ tctest.js
@@ -81,8 +81,11 @@ function key_tos (key) {
 
 var nonAuxKey_down = { code: KEY_S, value: 1 };
 var nonAuxKey_up = { code: KEY_S, value: 0 };
-var auxKey_down = { code: KEY_J, value: 1 };
-var auxKey_up = { code: KEY_J, value: 0 };
+var nonAuxKey_repeat = { code: KEY_S, value: 2 };
+var auxKey_down = { code: KEY_H, value: 1 };
+var auxKey_up = { code: KEY_H, value: 0 };
+var transAuxKey_down = { code: KEY_LEFT, value: 1 };
+var transAuxKey_up = { code: KEY_LEFT, value: 0 };
 var auxSwitch_down = { code: KEY_SPACE, value: 1 };
 var auxSwitch_up = { code: KEY_SPACE, value: 0 };
 
@@ -103,6 +106,45 @@ tc.reset();
 t(auxSwitch_down);
 t(nonAuxKey_down, [ auxSwitch_down, nonAuxKey_down ]);
 t(auxSwitch_up, [ auxSwitch_up ]); // abort aux mode and send aux switch
+t(nonAuxKey_up, [ nonAuxKey_up ]);
+
+tc.reset();
+t(nonAuxKey_down, [ nonAuxKey_down ]);
+t(auxSwitch_down);
+t(nonAuxKey_up, [nonAuxKey_up]);
+t(nonAuxKey_down, [ auxSwitch_down, nonAuxKey_down ]);
+t(nonAuxKey_up, [ nonAuxKey_up ]);
+t(auxSwitch_up, [ auxSwitch_up ]);
+		
+// wait for aux key up before emitting translation
+// to prevent accidentally emitting translations
+tc.reset();
+t(nonAuxKey_down, [ nonAuxKey_down ]);
+t(auxSwitch_down);
+t(auxKey_down);
+t(auxKey_up, [ transAuxKey_down, transAuxKey_up ]); 
+t(auxSwitch_up);
+
+tc.reset();
+t(nonAuxKey_down, [ nonAuxKey_down ]);
+t(auxSwitch_down);
+t(auxKey_down);
+t(auxSwitch_up, [ auxSwitch_down, auxKey_down, auxSwitch_up ]);
+t(auxKey_up, [ auxKey_up ]); 
+
+tc.reset();
+t(nonAuxKey_down, [ nonAuxKey_down ]);
+t(nonAuxKey_repeat, [ nonAuxKey_repeat ]);
+t(nonAuxKey_repeat, [ nonAuxKey_repeat ]);
+t(nonAuxKey_up, [ nonAuxKey_up ]);
+
+var otherAuxKey_down = { code: KEY_L, value: 1 };
+var otherAuxKey_up = { code: KEY_L, value: 0 };
+tc.reset();
+t(auxSwitch_down);
+t(auxKey_down);
+t(otherAuxKey_down); // another aux key down
+t(auxSwitch_up, [ auxSwitch_down, auxKey_down, otherAuxKey_down, auxSwitch_up ]);
 
 console.log (allpassed ? "Passed." : "Failed.");
 
